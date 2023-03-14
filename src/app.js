8d4faee0a871d3dcc4c5da7bab48f6dd1d723c98
santo0912 <santi.mendez.smz@gmail.com>
2023-03-14 01:31:52 +0000

    proyecto domain name generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big", "large"];
+  var noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}`);
+      }
+    }
+  }
 };
