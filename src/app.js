6fce170a3ca36ee7e96e42d33af56dc13bce7149
santo0912 <santi.mendez.smz@gmail.com>
2023-03-23 02:57:53 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-
-  var pronoun = ["the", "our"];
-  var adj = ["great", "big", "large"];
-  var noun = ["jogger", "racoon"];
-
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        console.log(`${pronoun[i]}${adj[j]}${noun[k]}`);
-      }
-    }
-  }
+const palos = ["spade", "club", "heart", "diamond"];
+const simbolos = {
+  spade: "♠︎",
+  club: "♣︎",
+  heart: "♥︎",
+  diamond: "♦︎"
 };
+const valores = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const cartaElement = document.querySelector(".card");
+const valorElement = document.querySelector(".value");
+const symbolTopLeftElement = document.querySelector(".symbol.top-left");
+const symbolBottomRightElement = document.querySelector(".symbol.bottom-right");
+
+function generarCarta() {
+  const palo = palos[Math.floor(Math.random() * palos.length)];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+
+  valorElement.innerHTML = valor;
+  symbolTopLeftElement.innerHTML = simbolos[palo];
+  symbolBottomRightElement.innerHTML = simbolos[palo];
+  cartaElement.classList.add(palo.toLowerCase());
+  cartaElement.style.borderRadius = "10px";
+  cartaElement.style.border = "2px solid black";
+}
+
+generarCarta();
